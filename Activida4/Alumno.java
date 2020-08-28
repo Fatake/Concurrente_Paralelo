18bea45485d27cecc425d001d2057c189df0b40a
katsushika <marcoderio850@gmail.com>
2020-08-28 10:10:23 -0500

    Primer edit (matriculas)

--- Activida4/Alumno.java
+++ Activida4/Alumno.java
@@ -1,9 +1,9 @@
 /*  Benemérita Universidad Autónoma de Puebla
     Programación Concurrente y Paralela
     Integrantes:
-        Arizmendi Ramírez Esiel Kevin
-        Coria Rios Marco Antonio
-        Ruiz Lozano Paulo Cesar
+        Arizmendi Ramírez Esiel Kevin, 2017
+        Coria Rios Marco Antonio, 201734576
+        Ruiz Lozano Paulo Cesar, 2017
  */
 
 public class Alumno {
