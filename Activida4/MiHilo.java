9051d0cd92b877a8683c164a35b5b12d4b7cb79a
Fatake Raicos <pace_lozano@hotmail.com>
2020-08-28 09:46:19 -0500

    Cambios en la estructura de MiHilo.java

--- Activida4/MiHilo.java
+++ Activida4/MiHilo.java
@@ -1,27 +1,37 @@
 class MiHilo extends Thread{
-   //Construye un nuevo hilo.
-    MiHilo(String nombre){
+    /**
+     * Constructor
+     * @param nombre
+     */
+    public MiHilo(String nombre){
     //super se usa para llamar a la versión del constructor de Thread
         super(nombre);
     }
-    //Punto de entrada del hilo
+
+    /**
+     * //Punto de entrada del hilo
+     */
     public void run(){
         System.out.println(getName()+" iniciando.");
-    //Como ExtendThread extiende de Thread, puede llamar directamente 
-    //a todos los métodos de Thread, incluido el método getName().
-    try {
-        for (int cont=0;cont<11;cont++){
-            Thread.sleep(5);
-            System.out.println("En "+getName()+ ", el recuento es "+cont);
+        //Como ExtendThread extiende de Thread, puede llamar directamente 
+        //a todos los métodos de Thread, incluido el método getName().
+        try {
+            for (int cont=0;cont<11;cont++){
+                Thread.sleep(5);
+                System.out.println("En "+getName()+ ", el recuento es "+cont);
+            }
+        }catch (InterruptedException exc){
+            System.out.println(getName()+ " interrumpido.");
         }
-    }catch (InterruptedException exc){
-        System.out.println(getName()+ " interrumpido.");
+            System.out.println(getName()+ " finalizando.");
     }
-        System.out.println(getName()+ " finalizando.");
-    }
-    public static void main(String args[])
-	   { 
-MiHilo miHilo=new MiHilo("Hola Mundo"); 
-miHilo.start();
-	   }
+    
+    /**
+     * Main
+     * @param args
+     */
+    public static void main(String args[]){ 
+        MiHilo miHilo=new MiHilo("Hola Mundo"); 
+        miHilo.start();
+	}
 }
