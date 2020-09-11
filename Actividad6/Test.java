c3dec3884ae5e1315b20cb5ec6f77d12e7ecddcb
Fatake <pace_lozano@hotmail.com>
2020-09-11 11:01:55 -0500

    Agregando generador de objetos

--- Actividad6/Test.java
+++ Actividad6/Test.java
@@ -1,10 +1,30 @@
+import java.util.Random;
+
 /**
  * 
  */
 public class Test{
-    public final int NUM_OBJETOS = 10;
-    private Rectangle[] objetos;
+    // Constante numero maximo de objetos
+    public final static int NUM_OBJETOS = 10;
+    // Arreglo de objetos
+    private static Rectangle[] objetos;
+    /**
+     * Generador de objetos Rectangulo
+     * @return
+     */
+    public Rectangle[] generaObjetos(){
+        Rectangle[] obs = new Rectangle[NUM_OBJETOS];
+        Random rand = new Random();
+        
+        for (int i = 0; i < NUM_OBJETOS; i++) {
+            obs[i] = new Rectangle("azul", rand.nextInt(16), rand.nextInt(16));
+        }
+        return obs;
+    }
     public static void main(String args[]){
-        System.out.println("Hola Mundo");
+        Test aux = new Test();
+        objetos = aux.generaObjetos();
+        
+        System.out.println("Hola Mundo"+);
     }
 }
